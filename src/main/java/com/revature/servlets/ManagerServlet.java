13133b396ea6139b21f92ca4321b028307cce7d7
Jan Balangue <jantonio.balangue@gmail.com>
2018-09-12 04:37:09 -0400

    Various modifications to ManagerServlet.java

--- src/main/java/com/revature/servlets/ManagerServlet.java
+++ src/main/java/com/revature/servlets/ManagerServlet.java
@@ -33,7 +33,10 @@ public class ManagerServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		String requestId = request.getParameter("requestId");
+		String approved = request.getParameter("approved");
+		
+		HttpSession session = request.getSession(false);
 	}
 
 }
